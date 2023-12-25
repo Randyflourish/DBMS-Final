856309326b9f73a2a78924cc983be5b05bd47cf6
Randyflourish <randylour1@gmail.com>
2023-12-25 14:44:42 +0800

    Add functions of favourite list

--- sql_command.py
+++ sql_command.py
@@ -1,6 +1,89 @@
 import mysql.connector
 import pandas as pd
 
+# Note: Please use string as all arguments' type
+
+def createFavouriteList(uid, listname):
+    mydb = mysql.connector.connect(
+        host="140.113.68.114",
+        port="3306",
+        user="manager",
+        password="toServer111550009",
+        database="db_final"
+    )
+    mycursor = mydb.cursor()
+    sql_command = "select max(listid) from favourite_list_data"
+    mycursor.execute(sql_command)
+    results = mycursor.fetchall()
+    mycursor.reset()
+    try:
+        lid = int(results[0][0]) + 1
+    except:
+        lid = 1
+    lid =str(lid)
+    mytup = (lid, listname, uid)
+    sql_command = "insert into favourite_list_data values (%s, %s, %s, NULL)"
+    mycursor.execute(sql_command, mytup)
+    mydb.commit()
+    mycursor.reset()
+    # check
+    sql_command = "select * from favourite_list_data"
+    mycursor.execute(sql_command)
+    results = mycursor.fetchall()
+    mycursor.reset()
+    for x in results:
+        print(x)
+    mycursor.close()
+    mydb.close()
+
+def deleteFavouriteList(listid):
+    mydb = mysql.connector.connect(
+        host="140.113.68.114",
+        port="3306",
+        user="manager",
+        password="toServer111550009",
+        database="db_final"
+    )
+    mycursor = mydb.cursor()
+    sql_command = "delete from favourite_list_data where listid = %s"
+    mytup = (listid, )
+    mycursor.execute(sql_command, mytup)
+    mydb.commit()
+    mycursor.reset()
+    # check
+    sql_command = "select * from favourite_list_data where listid = %s"
+    mycursor.execute(sql_command, mytup)
+    results = mycursor.fetchall()
+    mycursor.reset()
+    for x in results:
+        print(x)
+    mycursor.close()
+    mydb.close()
+
+def deleteUserFavouriteLists(uid):
+    mydb = mysql.connector.connect(
+        host="140.113.68.114",
+        port="3306",
+        user="manager",
+        password="toServer111550009",
+        database="db_final"
+    )
+    mycursor = mydb.cursor()
+    sql_command = "delete from favourite_list_data where userid = %s"
+    mytup = (uid, )
+    mycursor.execute(sql_command, mytup)
+    mydb.commit()
+    mycursor.reset()
+    # check
+    sql_command = "select * from favourite_list_data where userid = %s"
+    mycursor.execute(sql_command, mytup)
+    results = mycursor.fetchall()
+    mycursor.reset()
+    for x in results:
+        print(x)
+    mycursor.close()
+    mydb.close()
+
 def createUserAccount(userName, userPass):
     mydb = mysql.connector.connect(
         host="140.113.68.114",
@@ -71,6 +154,7 @@ def deleteUserAccount(userName, userPass):
         mycursor.close()
         mydb.close()
         return
+    deleteUserFavouriteLists(uid)
     mytup = (uid, )
     sql_command = "delete from user_data where userid = %s"
     mycursor.execute(sql_command, mytup)
@@ -86,3 +170,10 @@ def deleteUserAccount(userName, userPass):
     mycursor.close()
     mydb.close()
 
+"""
+testing data
+createUserAccount("A","0800000123")
+createFavouriteList("1","MYT")
+deleteUserAccount("A","0800000123")
+
+"""
