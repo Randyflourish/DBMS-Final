69693ef8e83d7fd5753b0d82ba3fcce25802be79
Randyflourish <randylour1@gmail.com>
2024-01-02 12:27:07 +0800

    add auto increment reset func. add app to FList func without testing

--- sql_command.py
+++ sql_command.py
@@ -10,15 +10,24 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
+def resetAI(tablename):
+    global mydb, mycursor
+    sql_command = "ALTER TABLE "+tablename+" AUTO_INCREMENT = 1"
+    mycursor.execute(sql_command)
+    mydb.commit()
+    mycursor.reset()
+
 # (id): new list id
 def createFList(uid, listname):
     global mydb, mycursor
+    if type(uid) != str:
+        uid = str(uid)
     mytup = (listname, uid)
-    sql_command = "insert into flist_conn_data (listname, userid) values (%s, %s)"
+    sql_command = "INSERT INTO flist_conn_data (listname, userid) VALUES (%s, %s)"
     mycursor.execute(sql_command, mytup)
     mydb.commit()
     mycursor.reset()
-    sql_command = "select last_insert_id()"
+    sql_command = "SELECT last_insert_id()"
     mycursor.execute(sql_command)
     results = mycursor.fetchall()
     mycursor.reset()
@@ -27,12 +36,14 @@ def createFList(uid, listname):
 
 def deleteFList(listid):
     global mydb, mycursor
+    if type(listid) != str:
+        listid = str(listid)
     mytup = (listid, )
-    sql_command = "delete from flist_data where listid = %s"
+    sql_command = "DELETE FROM flist_data WHERE listid = %s"
     mycursor.execute(sql_command, mytup)
     mydb.commit()
     mycursor.reset()
-    sql_command = "delete from flist_conn_data where listid = %s"
+    sql_command = "DELETE FROM flist_conn_data WHERE listid = %s"
     mycursor.execute(sql_command, mytup)
     mydb.commit()
     mycursor.reset()
@@ -40,7 +51,7 @@ def deleteFList(listid):
 def deleteUserFLists(uid):
     global mydb, mycursor
     mytup = (uid, )
-    sql_command = "select listid from flist_conn_data where userid = %s"
+    sql_command = "SELECT listid FROM flist_conn_data WHERE userid = %s"
     mycursor.execute(sql_command, mytup)
     results = mycursor.fetchall()
     mycursor.reset()
@@ -48,36 +59,75 @@ def deleteUserFLists(uid):
         try:
             lid = int(col[0])
             mytup = (lid, )
-            sql_command = "delete from flist_data where listid = %s"
+            sql_command = "DELETE FROM flist_data WHERE listid = %s"
             mycursor.execute(sql_command, mytup)
             mydb.commit()
             mycursor.reset()
         except:
             break
     mytup = (uid, )
-    sql_command = "delete from flist_conn_data where userid = %s"
+    sql_command = "DELETE FROM flist_conn_data WHERE userid = %s"
     mycursor.execute(sql_command, mytup)
     mydb.commit()
     mycursor.reset()
 
+# 0: already in this list 1: success
+def insertAppIntoFList(appid, listid):
+    global mydb, mycursor
+    if type(appid) != str:
+        appid = str(appid)
+    if type(listid) != str:
+        listid = str(listid)
+    mytup = (listid, appid)
+    sql_command = "SELECT COUNT(*) FROM flist_data WHERE listid = %s AND appid = %s"
+    mycursor.execute(sql_command, mytup)
+    results = mycursor.fetchall()
+    cnt = int(results[0][0])
+    if cnt != 0:
+        return 0
+    sql_command = "INSERT INTO flist_data VALUES (%s, %s)"
+    mycursor.execute(sql_command, mytup)
+    mydb.commit()
+    mycursor.reset()
+    return 1
+
+# 0: not in this list 1: success
+def deleteAppFromFList(appid, listid):
+    global mydb, mycursor
+    if type(appid) != str:
+        appid = str(appid)
+    if type(listid) != str:
+        listid = str(listid)
+    mytup = (listid, appid)
+    sql_command = "SELECT COUNT(*) FROM flist_data WHERE listid = %s AND appid = %s"
+    mycursor.execute(sql_command, mytup)
+    results = mycursor.fetchall()
+    cnt = int(results[0][0])
+    if cnt != 0:
+        return 0
+    sql_command = "DELETE FROM flist_data WHERE listid = %s AND appid = %s"
+    mycursor.execute(sql_command, mytup)
+    mydb.commit()
+    mycursor.reset()
+    return 1
+
 # -1: account has been used (id): new account id
 def createUserAccount(userName, userPass):
     global mydb, mycursor
-    sql_command = "select count(username) from user_data where username = %s"
+    sql_command = "SELECT COUNT(username) FROM user_data WHERE username = %s"
     mytup = (userName, )
     mycursor.execute(sql_command, mytup)
     results = mycursor.fetchall()
     mycursor.reset()
     cnt = int(results[0][0])
     if cnt != 0:
-        print("This username has been used\nPlease choose another one.")
         return -1
     mytup = (userName, userPass)
-    sql_command = "insert into user_data (username, userpassword) values (%s, %s)"
+    sql_command = "INSERT INTO user_data (username, userpassword) VALUES (%s, %s)"
     mycursor.execute(sql_command, mytup)
     mydb.commit()
     mycursor.reset()
-    sql_command = "select last_insert_id()"
+    sql_command = "SELECT last_insert_id()"
     mycursor.execute(sql_command)
     results = mycursor.fetchall()
     mycursor.reset()
@@ -87,7 +137,9 @@ def createUserAccount(userName, userPass):
 # -1: account does not exist 0: password fail 1: success
 def deleteUserAccount(uid, userPass):
     global mydb, mycursor
-    sql_command = "select * from user_data where userid = %s"
+    if type(uid) != str:
+        uid = str(uid)
+    sql_command = "SELECT * FROM user_data WHERE userid = %s"
     mytup = (uid, )
     mycursor.execute(sql_command, mytup)
     results = mycursor.fetchall()
@@ -95,14 +147,12 @@ def deleteUserAccount(uid, userPass):
     try:
         password = str(results[0][2])
     except:
-        print("Account does not exist.\nPlease try again.")
         return -1
     if userPass != password:
-        print("Password Incorrect.\nPlease try again.")
         return 0
     deleteUserFLists(uid)
     mytup = (uid, )
-    sql_command = "delete from user_data where userid = %s"
+    sql_command = "DELETE FROM user_data WHERE userid = %s"
     mycursor.execute(sql_command, mytup)
     mydb.commit()
     mycursor.reset()
@@ -111,7 +161,7 @@ def deleteUserAccount(uid, userPass):
 # -1: account does not exist 0: password fail (id): login
 def login(userName, userPass):
     global mydb, mycursor
-    sql_command = "select * from user_data where username = %s"
+    sql_command = "SELECT * FROM user_data WHERE username = %s"
     mytup = (userName, )
     mycursor.execute(sql_command, mytup)
     results = mycursor.fetchall()
@@ -120,18 +170,20 @@ def login(userName, userPass):
         id = int(results[0][0])
         password = str(results[0][2])
     except:
-        print("Account does not exist.\nPlease try again.")
         return -1
-    if userPass == password:
-        print("Success.\n")
-        return id
-    else:
-        print("Password Incorrect.\nPlease try again.")
+    if userPass != password:
         return 0
+    return id
 
 """
 testing data
 createUserAccount("A","0800000123")
-deleteUserAccount("1","0800000123")
+deleteUserAccount(uid,"0800000123")
 createFList(str(uid),"MHY")
+resetAI("user_data")
+resetAI("flist_conn_data")
 """
+
+
+
+
