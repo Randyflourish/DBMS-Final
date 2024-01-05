d8cdc98f4f777eb35d6fce636f646a4d1106306c
Huang Che Yu <jerry378jerry@gmail.com>
2024-01-06 02:06:26 +0800

    login window

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var login = document.getElementById('login');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == login) {
+        login.style.display = "none";
+    }
+}
+
+
+
