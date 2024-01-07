b139fd0bfcea0afb3288528985e59fcafb2109dd
Huang Che Yu <jerry378jerry@gmail.com>
2024-01-08 03:28:05 +0800

    prelimiary all of the page and functions

--- js/index.js
+++ js/index.js
@@ -1,19 +1,7 @@
-var login = document.getElementById('login');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == login) {
-        login.style.display = "none";
-    }
+function ShowBlock(BlockName) {
+    document.getElementById(BlockName).style.display='block';
 }
-
-var signup = document.getElementById('signup');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == signup) {
-        signup.style.display = "none";
-    }
+function CloseBlock(BlockName) {
+    document.getElementById(BlockName).style.display='none';
 }
-
 
