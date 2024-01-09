49af7bf64f712081dc81b883bc364f6e34d29666
Randyflourish <randylour1@gmail.com>
2024-01-09 16:24:34 +0800

    A big update

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function ShowBlock(BlockName) {
-    document.getElementById(BlockName).style.display='block';
-}
-function CloseBlock(BlockName) {
-    document.getElementById(BlockName).style.display='none';
-}
-
